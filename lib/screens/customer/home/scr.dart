3e833f80038cb82298d3114e217096c300e0af38
Simon Gin <tt26419@gmail.com>
2024-12-05 11:20:22 +0700

    setup: add animation to the customer router

--- lib/screens/customer/home/scr.dart
+++ lib/screens/customer/home/scr.dart
@@ -5,6 +5,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Text("Home"));
+    return const SafeArea(
+        child: Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "FlockStay",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            Column(children: [
+              Text("Current Location"),
+              Row(children: [
+                Icon(Icons.location_on),
+              ])
+            ])
+          ],
+        ),
+      ],
+    ));
   }
 }
--- lib/screens/customer/layout.dart
+++ lib/screens/customer/layout.dart
@@ -1,9 +1,12 @@
 import 'package:flockstay_mobile/constants/colors.dart';
+import 'package:flockstay_mobile/screens/customer/home/scr.dart';
+import 'package:flockstay_mobile/screens/customer/notifications/scr.dart';
+import 'package:flockstay_mobile/screens/customer/profile/scr.dart';
+import 'package:flockstay_mobile/screens/customer/reservations/scr.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class CustomerLayout extends StatelessWidget {
+class CustomerLayout extends StatefulWidget {
   final Widget childBody;
   final String currentScreen;
 
@@ -11,71 +14,108 @@ class CustomerLayout extends StatelessWidget {
       {super.key, required this.childBody, required this.currentScreen});
 
   @override
+  State<CustomerLayout> createState() => _CustomerLayoutState();
+}
+
+class _CustomerLayoutState extends State<CustomerLayout> {
+  late PageController _pageController;
+  int _currentIndex = 0;
+
+  final List<String> _routes = [
+    "/home",
+    "/reservations",
+    "/notifications",
+    "/profile",
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = _routes.indexOf(widget.currentScreen);
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void didUpdateWidget(CustomerLayout oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final newIndex = _routes.indexOf(widget.currentScreen);
+    if (newIndex != _currentIndex) {
+      _currentIndex = newIndex;
+      _pageController
+          .jumpToPage(_currentIndex); // Ensure the PageView is synchronized
+    }
+  }
+
+  void _onItemTapped(int index) {
+    if (_currentIndex != index) {
+      // Update the URL via GoRouter
+      context.go(_routes[index]);
+
+      // Animate to the selected page
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+
+      // Update the current index
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: childBody,
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          context.go(_routes[index]); // Update URL on swipe
+        },
+        children: const [
+          HomeScreen(),
+          ReservationsScreen(),
+          NotificationsScreen(),
+          ProfileScreen(),
+        ],
+      ),
       bottomNavigationBar: Container(
-        padding: const EdgeInsets.only(bottom: 20 / 2),
+        padding: const EdgeInsets.only(bottom: 10),
         height: 80,
-        decoration: BoxDecoration(color: Colors.white, boxShadow: [
-          BoxShadow(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
               offset: const Offset(0, -10),
               blurRadius: 35,
-              color: flockCyan.withOpacity(0.38))
-        ]),
+              color: Colors.cyan.withOpacity(0.38),
+            )
+          ],
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            IconButton(
-                onPressed: () {
-                  if (currentScreen != "/home") {
-                    context.goNamed("home");
-                  }
-                },
-                icon: Icon(
-                  MdiIcons.home,
-                  size: 30,
-                  color: currentScreen == "/home" ? flockCyan : Colors.black,
-                )),
-            IconButton(
-                onPressed: () {
-                  if (currentScreen != "/reservations") {
-                    context.goNamed("reservations");
-                  }
-                },
-                icon: Icon(
-                  MdiIcons.listBox,
-                  size: 30,
-                  color: currentScreen == "/reservations"
-                      ? flockCyan
-                      : Colors.black,
-                )),
-            IconButton(
-                onPressed: () {
-                  if (currentScreen != "/notifications") {
-                    context.goNamed("notifications");
-                  }
-                },
-                icon: Icon(
-                  MdiIcons.bell,
-                  size: 30,
-                  color: currentScreen == "/notifications"
-                      ? flockCyan
-                      : Colors.black,
-                )),
-            IconButton(
-                onPressed: () {
-                  if (currentScreen != "/profile") {
-                    context.goNamed("profile");
-                  }
-                },
-                icon: Icon(
-                  MdiIcons.account,
-                  size: 30,
-                  color: currentScreen == "/profile" ? flockCyan : Colors.black,
-                )),
+            _buildBottomNavItem(0, Icons.home),
+            _buildBottomNavItem(1, Icons.list),
+            _buildBottomNavItem(2, Icons.notifications),
+            _buildBottomNavItem(3, Icons.person),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildBottomNavItem(int index, IconData icon) {
+    return IconButton(
+      onPressed: () => _onItemTapped(index),
+      icon: Icon(
+        icon,
+        size: 30,
+        color: _currentIndex == index ? flockCyan : Colors.black,
       ),
     );
   }
