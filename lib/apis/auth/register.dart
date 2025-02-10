e9966eff45af12bf859f342bec27a42be956921b
Simon Gin <tt26419@gmail.com>
2025-02-10 10:25:11 +0700

    ui: integrate the quickalert diaglog for app

--- lib/apis/auth/register.dart
+++ lib/apis/auth/register.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: avoid_print
-
 import 'package:dio/dio.dart';
+import 'package:flockstay_mobile/routes/index.dart';
 import 'package:flockstay_mobile/services/dio_client.dart';
+import 'package:quickalert/quickalert.dart';
+import 'package:go_router/go_router.dart';
 
 Future<void> register(String phone, String name, String password) async {
   Dio dio = DioClient().dio;
@@ -9,12 +11,30 @@ Future<void> register(String phone, String name, String password) async {
   try {
     Response response = await dio.post("/auth/register",
         data: {"phone": phone, "username": name, "password": password});
+    if (navigatorKey.currentContext == null) {
+      print("Navigator context is null! Cannot show alert.");
+      return;
+    }
     if (response.statusCode == 200) {
-      print("Registration successful!");
+      QuickAlert.show(
+        context: navigatorKey.currentContext!,
+        type: QuickAlertType.success,
+        text: "Registration successful!",
+        onConfirmBtnTap: () =>
+            {GoRouter.of(navigatorKey.currentContext!).goNamed("login")},
+      );
     } else if (response.statusCode == 409) {
-      print("Phone number already in use!");
+      QuickAlert.show(
+        context: navigatorKey.currentContext!,
+        type: QuickAlertType.error,
+        text: "Phone number already registered!",
+      );
     } else {
-      print("Registration failed!");
+      QuickAlert.show(
+        context: navigatorKey.currentContext!,
+        type: QuickAlertType.error,
+        text: "Something went wrong! Please try again later.",
+      );
     }
   } catch (e) {
     print("Error: $e");
--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flockstay_mobile/screens/auth/login/scr.dart';
 import 'package:flockstay_mobile/screens/auth/register/scr.dart';
 import 'package:flockstay_mobile/screens/customer/home/scr.dart';
@@ -7,37 +8,43 @@ import 'package:flockstay_mobile/screens/customer/profile/scr.dart';
 import 'package:flockstay_mobile/screens/customer/reservations/scr.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter appRouter = GoRouter(initialLocation: "/auth/login", routes: [
-  GoRoute(
-      name: "login",
-      path: "/auth/login",
-      builder: (context, state) => LoginScreen()),
-  GoRoute(
-      name: "register",
-      path: "/auth/register",
-      builder: (context, state) => RegisterScreen()),
-  ShellRoute(
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+final GoRouter appRouter = GoRouter(
+    navigatorKey: navigatorKey,
+    initialLocation: "/auth/login",
     routes: [
       GoRoute(
-          name: "home",
-          path: "/home",
-          builder: (context, state) => const HomeScreen()),
+          name: "login",
+          path: "/auth/login",
+          builder: (context, state) => LoginScreen()),
       GoRoute(
-          name: "reservations",
-          path: "/reservations",
-          builder: (context, state) => const ReservationsScreen()),
-      GoRoute(
-          name: "notifications",
-          path: "/notifications",
-          builder: (context, state) => const NotificationsScreen()),
-      GoRoute(
-          name: "profile",
-          path: "/profile",
-          builder: (context, state) => const ProfileScreen()),
-    ],
-    builder: (context, state, child) => CustomerLayout(
-      childBody: child,
-      currentScreen: state.matchedLocation,
-    ),
-  )
-]);
+          name: "register",
+          path: "/auth/register",
+          builder: (context, state) => RegisterScreen()),
+      ShellRoute(
+        navigatorKey: navigatorKey,
+        routes: [
+          GoRoute(
+              name: "home",
+              path: "/home",
+              builder: (context, state) => const HomeScreen()),
+          GoRoute(
+              name: "reservations",
+              path: "/reservations",
+              builder: (context, state) => const ReservationsScreen()),
+          GoRoute(
+              name: "notifications",
+              path: "/notifications",
+              builder: (context, state) => const NotificationsScreen()),
+          GoRoute(
+              name: "profile",
+              path: "/profile",
+              builder: (context, state) => const ProfileScreen()),
+        ],
+        builder: (context, state, child) => CustomerLayout(
+          childBody: child,
+          currentScreen: state.matchedLocation,
+        ),
+      )
+    ]);
