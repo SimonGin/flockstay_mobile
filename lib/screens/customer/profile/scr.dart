a40e0d357fe56a90984ef00fa72a561a9a35335f
Simon Gin <tt26419@gmail.com>
2025-02-10 21:39:40 +0700

    ui: customer's profile screen with logout function

--- lib/screens/customer/profile/scr.dart
+++ lib/screens/customer/profile/scr.dart
@@ -1,10 +1,56 @@
+import 'package:flockstay_mobile/constants/colors.dart';
+import 'package:flockstay_mobile/screens/customer/profile/profile_btn.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Text("Profile"));
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+      child: Column(children: [
+        Icon(
+          MdiIcons.accountCircle,
+          size: 200,
+          color: flockCyan,
+        ),
+        const Text("Simon Gin",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+        const SizedBox(height: 30),
+        Wrap(
+          runSpacing: 20,
+          children: [
+            ProfileBtn(
+              icon: MdiIcons.heart,
+              title: "Favourites",
+              onNavi: () {},
+            ),
+            ProfileBtn(
+                icon: MdiIcons.accountEdit,
+                title: "Edit Profile",
+                onNavi: () {}),
+            ProfileBtn(
+                icon: MdiIcons.lockReset,
+                title: "Reset Password",
+                onNavi: () {}),
+            ProfileBtn(
+                icon: MdiIcons.logout,
+                title: "Logout",
+                onNavi: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  await prefs.remove("access_token");
+                  if (!context.mounted) return;
+                  context.goNamed("login");
+                }),
+          ],
+        )
+      ]),
+    ));
   }
 }
